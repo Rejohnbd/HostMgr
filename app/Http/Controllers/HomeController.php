b663fdf69d0e7f66ce9804c8aaf5740158b0a095
rejohnbd <rejohn.cse@gmail.com>
2022-04-09 04:10:20 +0600

    Stop Login without admin type

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,7 +24,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        // return view('home');
-        return redirect()->action('DashboardController@index');
+        if (Auth::user()->type == 'admin') :
+            return redirect()->action('DashboardController@index');
+        else :
+            Auth::logout();
+            return redirect('/');
+        endif;
     }
 }
